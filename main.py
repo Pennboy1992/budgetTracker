4ae10a6c040df5ba0f9b78cfb0c678e8815f1615
Roman Penn <pnnroman92@gmail.com>
2022-03-07 19:05:26 -0600

    got initial logic working

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
-# This is a sample Python script.
+def main():
+    budget = []
+    print("Welcome to Budget Tracking with Roman")
+    while True:
+        try:
+            new_item = int(input("Do you wish you enter a new item: \n1.Yes\n2.No\n"))
+        except ValueError:
+            print("Please enter a valid choice (1 or 2)")
+            new_item = int(input("Do you wish you enter a new item: \n1.Yes\n2.No\n"))
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+        if new_item == 1:
+            budget_item(budget)
+        else:
+            print("Here is your current items: ")
+            print(budget)
+            break
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def budget_item(arr):
+    print("Enter a new item you wish to be added to your budget.")
+    item_name = input("Enter the name for your item: ")
+    try:
+        income_or_debit = int(input("Is this income or a debit:\n1.Income\n2.Debit\nEnter Corresponding Number: "))
+    except ValueError:
+        print("Please enter a valid choice (1 or 2)")
+        income_or_debit = int(input("Is this income or a debit:\n1.Income\n2.Debit\nEnter Corresponding Number: "))
+
+    print(item_name)
+    print(income_or_debit)
+    if income_or_debit == 1:
+        pay = float(input("How much will you be making from this source of income this month: "))
+    else:
+        pay = float(input("How much is this expense costing you per month: "))
+    arr.append({"item": item_name, "type": "Income" if income_or_debit == 1 else "Expense", "amount": round(pay, 2)})
+    return arr
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
