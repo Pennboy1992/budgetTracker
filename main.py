94c7530f91b1c8fd49dd6cfc2fd1265e272b3eba
Roman Penn <pnnroman92@gmail.com>
2022-03-10 20:39:20 -0600

    updated multiple functions to use tabulate and the requirement.txt to include pyfiglet

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import pyexcel
 from tabulate import tabulate
+import pyfiglet
 
 
 def main():
     budget = []
-    print("Welcome to Budget Tracking with Roman")
+    welcome_text = pyfiglet.figlet_format("Budget Tracker", font="slant")
+    print(welcome_text)
     while True:
         try:
             new_item = int(input("What would you like to do?: \n1. Enter a new item\n2. Update an item\n3. Remove an "
-                                 "item \n4. Print Stats\n5. Quit\n"))
+                                 "item \n4. Print Stats\n5. Quit\nEnter Corresponding Number: "))
         except ValueError:
             print("Please enter a valid choice (1 or 2)")
             new_item = int(input("What would you like to do?: \n1. Enter a new item\n 2. Update an item\n3. Remove an "
-                                 "item \n4. Print Stats \n5. Quit"))
+                                 "item \n4. Print Stats \n5. Quit\nEnter Corresponding Number: "))
 
         if new_item == 1:
             budget_item(budget)
@@ -121,29 +123,33 @@ def remove_item(arr):
 
 
 def stats(arr):
-    total_income = 0
-    total_expenses = 0
-    for item in arr:
-        if item['Type'] == 'Income':
-            total_income += item['Amount']
-        else:
-            total_expenses += item['Amount']
+    if len(arr) >= 2:
+        total_income = 0
+        total_expenses = 0
+        for item in arr:
+            if item['Type'] == 'Income':
+                total_income += item['Amount']
+            else:
+                total_expenses += item['Amount']
 
-    amount_list_income = [item['Amount'] for item in arr if item['Type'] == 'Income']
-    amount_list_expense = [item['Amount'] for item in arr if item['Type'] == 'Expense']
+        amount_list_income = [item['Amount'] for item in arr if item['Type'] == 'Income']
+        amount_list_expense = [item['Amount'] for item in arr if item['Type'] == 'Expense']
 
-    highest_income = list(filter(lambda x: x['Amount'] == max(amount_list_income), arr))[0]
-    highest_expense = list(filter(lambda x: x['Amount'] == max(amount_list_expense), arr))[0]
+        highest_income = list(filter(lambda x: x['Amount'] == max(amount_list_income), arr))[0]
+        highest_expense = list(filter(lambda x: x['Amount'] == max(amount_list_expense), arr))[0]
 
-    print("These are your current items: \n")
-    print(table(arr))
+        print("These are your current items: \n")
+        print(table(arr))
 
-    print(f"Your total income this month is: ${total_income: .2f}.")
-    print(f"Your total expenses this months is: ${total_expenses: .2f}.")
-    print(
-        f"Your highest source of income is your {highest_income['Item']} at ${highest_income['Amount']:.2f} per month.")
-    print(f"Your highest expense is your {highest_expense['Item']} which is costing ${highest_expense['Amount']:.2f} "
-          f"per month.")
+        print(f"Your total income this month is: ${total_income: .2f}.")
+        print(f"Your total expenses this months is: ${total_expenses: .2f}.")
+        print(
+            f"Your highest source of income is your {highest_income['Item']} at ${highest_income['Amount']:.2f} per month.")
+        print(
+            f"Your highest expense is your {highest_expense['Item']} which is costing ${highest_expense['Amount']:.2f} "
+            f"per month.")
+    else:
+        print("We do not have enough data at this time to show budget statistics at this time.\n\n")
 
 
 def table(arr_to_table):
